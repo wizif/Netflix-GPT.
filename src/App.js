9ab5db02e1531dbc613f330e0e9651925b9f5842
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-07-03 10:56:12 +0530

    sign in/up form built

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-// import logo from './logo.svg;
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
   <>
-  <div className="text-3xl font-bold text-green-800">
-    Hello every you
-you
-  </div>
+<Body/>
   </>
   );
 }
