b3d94d3302230669dd43a278dcdd177dc63d546a
eezlogicxarvind <159049532+eezlogicxarvind@users.noreply.github.com>
2025-07-08 15:53:34 +0530

    added utils

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 //  https://netflix-gpt-fe7f2.web.app
 .
+AIzaSyCsy-fO-4yTjeo2pvpI8k3NymSEGxMJj_I
--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,10 +1,11 @@
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
-import MainContainer from "./MainContainer";
+// import MainContainer from "./MainContainer.js";
 import SecondaryContainer from "./SecondaryContainer";
 import usePopularMovies from "../hooks/usePopularMovies";
 import GptSearch from "./GptSearch";
 import { useSelector } from "react-redux";
+import MainContainer from "./Maincontainer";
 
 const Browse = () => {
   const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
@@ -19,7 +20,7 @@ const Browse = () => {
         <GptSearch />
       ) : (
         <>
-          <MainContainer />
+          <MainContainer/>
           <SecondaryContainer />
         </>
       )}
